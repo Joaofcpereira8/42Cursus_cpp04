e026a6f40704d8a582598cbb395f136d7d90ef64
Joao's Mac <joaofcpereira8@gmail.com>
2024-08-12 20:56:04 +0100

    added Character cpp and hpp

---
+++ ex03/Character.cpp
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Character.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jofilipe@student.42porto.com <jofilipe>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/12 20:33:21 by jofilipe@st       #+#    #+#             */
+/*   Updated: 2024/08/12 20:54:12 by jofilipe@st      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Character.hpp"
+
+Character::Character() {
+	std::cout << "Character default constructor called" << std::endl;
+}
+
---
+++ ex03/Character.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Character.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jofilipe@student.42porto.com <jofilipe>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/12 20:27:35 by jofilipe@st       #+#    #+#             */
+/*   Updated: 2024/08/12 20:42:50 by jofilipe@st      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CHARACTER_HPP
+# define CHARACTER_HPP
+
+# include "ICharacter.hpp"
+
+class Character: public ICharacter {
+	public:
+		Character();
+		Character(std::string charName);
+		Character(const Character& idemName);
+		Character &operator=(const Character& copyCharec);
+		~Character();
+};
+
+#endif
